96e159666b7a607b2a47c36ef886a3d0ee24f77f
Javier Ballón <javi4195@gmail.com>
2024-04-26 16:47:33 -0300

    New view was created for EspecialidadController

--- Turnos/Controllers/EspecialidadController.cs
+++ Turnos/Controllers/EspecialidadController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Turnos.Models;
 
 namespace Turnos.Controllers
 {
     public class EspecialidadController : Controller
     {
-        public EspecialidadController()
-        {
 
+        private readonly TurnosContext _context;
+
+        public EspecialidadController(TurnosContext context)
+        {
+            _context = context;
         }
 
         public IActionResult Index() 
         {
-            return View();
+            return View(_context.Especialidad.ToList());
         }
     }
 }
