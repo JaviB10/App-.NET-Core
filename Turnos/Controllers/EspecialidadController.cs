d94130e0e72e5c6c0b1908feb750d46f2226c474
Javier Ballón <javi4195@gmail.com>
2024-04-28 19:15:06 -0300

    Add new metods for PacienteController

--- Turnos/Controllers/EspecialidadController.cs
+++ Turnos/Controllers/EspecialidadController.cs
@@ -86,7 +86,7 @@ namespace Turnos.Controllers
             }
             _context.Especialidad.Remove(especialidad);
             
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
         }
--- Turnos/Controllers/PacienteController.cs
+++ Turnos/Controllers/PacienteController.cs
@@ -21,5 +21,117 @@ namespace Turnos.Controllers
         {
             return View(await _context.Paciente.ToListAsync());
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.PacienteID == id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Valida que nuestro metodo create, ha sido ejecutado a traves del formulario y no ha sido ejecutado mediante la URL de la aplicacion
+        public async Task<IActionResult> Create([Bind("PacienteID,Nombre,Apellido,Domicilio,Telefono,Email")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(paciente);
+        }
+
+         public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FindAsync(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PacienteID,Nombre,Apellido,Domicilio,Telefono,Email")] Paciente paciente)
+        {
+            if (id != paciente.PacienteID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(paciente);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(paciente);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(e => e.PacienteID == id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FindAsync(id);
+
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            _context.Paciente.Remove(paciente);
+            
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
